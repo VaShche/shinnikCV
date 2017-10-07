20a20b9b8e0520083e6ce194d2bf54e1e712593f
kozlov-a <kozlov.alexandr.85@gmail.com>
2017-10-07 19:10:27 +0300

    add  class for DetectorRoadSign ClassificatorRoadSign

---
+++ src/classificatorRoadSign.cpp
@@ -0,0 +1,13 @@
+
+#include "ClassificatorRoadSign.hpp"
+
+int Shinik::ClassificatorRoadSign::Init(const std::string& path) {
+    //TODO:
+    return 0;
+}
+
+int  Shinik::ClassificatorRoadSign::Process(const Mat& image, std::vector<Mat>& outSign ) const {
+    //TODO:
+    return 0;
+
+}
---
+++ src/detectorRoadSign.cpp
@@ -0,0 +1,12 @@
+#include "detectorRoadSign.hpp"
+
+int Shinik::DetectorRoadSign::Init(const std::string& path) {
+
+    return 0;
+}
+
+Shinik::Sign Shinik::DetectorRoadSign::Process(const Mat& image, std::vector<Mat>& outSign) const {
+    Shinik::Sign res;
+    
+    return res;
+}
