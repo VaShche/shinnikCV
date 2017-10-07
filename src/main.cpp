d00ec478dbdc3181a46332f6136f1673e6193ab1
kozlov-a <kozlov.alexandr.85@gmail.com>
2017-10-07 17:48:08 +0300

    stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
+#include <iostream>
 #include <fstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 
+using namespace cv;
+using namespace std;
 int main(int argc, char * argv[]) {
+
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+
+    if (!image.data)                              // Check for invalid input
+    {
+        cout << "Could not open or find the image" << std::endl;
+        return -1;
+    }
 
     std::ofstream outFile("results.csv");
     outFile << "rr";
