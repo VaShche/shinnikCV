6ec6bdb2f4e73abff6a22409f5d6db4b7dbca8b8
Ilya Kalinovskii <kua_21@mail.ru>
2017-10-08 10:13:24 +0300

    HOG+SVM+CNN

---
+++ transito-cv/src/hr_timer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <windows.h>
+
+
+//================================================================================================================================================
+
+
+class Timer
+{
+private:
+	unsigned long long* counters;
+	unsigned long long  freq;
+
+public:
+	Timer(int = 1, bool = false);
+	~Timer(void);
+
+	void start(int = 0);
+	double get(double = 1., int = 0);
+};
